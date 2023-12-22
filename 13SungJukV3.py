8a455b8a1443610314d56956368520fe4e36ee4b
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2023-12-22 18:11:07 +0900

    성적처리 프로그램 V3 - CRUD 1

--- 13SungJukV3.py
+++ 13SungJukV3.py
@@ -2,12 +2,9 @@
 # 이름, 국어, 영어, 수학을 이용해서
 # 총점, 평균, 학점을 계산하고 출력함
 # 학점 기준 : 수우미양가
-# 데이터 입력 시 input 함수 이용
-names= input("학생 이름을 입력하세요")
-kors = int(input("국어 점수를 입력하세요."))
-engs = int(input("영어 점수를 입력하세요."))
-mats = int(input("수학 점수를 입력하세요."))
-
+# 성적 입력, 조회, 상세 조회, 수정, 삭제 기능 구현
+# 각 기능은 메뉴식으로 구현 - 기능별 메뉴 선택 시 명령 수행
+import sys
 
 names = []
 kors = []
@@ -17,32 +14,58 @@ tots = []
 avgs = []
 grds = []
 
-# 성적 데이터 입력
-for i in range(3):
-    print(f'{i+1}번째 학생 데이터 입력')
-    names.append(input('이름은 ? : '))
-    kors.append(int(input('국어 점수는 ? : ')))
-    engs.append(int(input('영어 점수는 ? : ')))
-    mats.append(int(input('수학 점수는 ? : ')))
+# 프로그램 메뉴 출력을 위한 변수 선언
+main_menu = '''
+성적처리 프로그램 V3
+-----------------
+1. 성적 데이터 추가
+2. 성적 데이터 조회
+3. 성적 데이터 상세 조회
+4. 성적 데이터 수정
+5. 성적 데이터 삭제
+0. 프로그램 종료
+-----------------
+'''
+for i in range(100):
+    # 프로그램 주 실행부
+    print(main_menu, end ='')
+    menu = input('=> 메뉴를 선택하세요 : ')
+
+# 메뉴 선택에 따라 해당 기능 수행
+    if menu == '1':
+        print('성적 데이터 추가')
+        sjpos = len(avgs) # 현재 입력된 데이터 수
+        names.append(input('이름은 ? : '))
+        kors.append(int(input('국어 점수는? : ')))
+        engs.append(int(input('영어 점수는? : ')))
+        mats.append(int(input('수학 점수는? : ')))
+
+        tots.append(kors[sjpos] + engs[sjpos] + mats[sjpos])
+        avgs.append(tots[sjpos] / 3)
+        avg = avgs[sjpos]
+        grd = '수' if avg >= 90 else \
+        '우' if avg >= 80 else \
+        '미' if avg >= 70 else \
+        '양' if avg >= 60 else '가'
+        grds.append(grd)
+
+    elif menu == '2':
+        print('성적 데이터 조회')
+        for i in range(len(names)):
+            print(f'이름 : {names[i]:s}, 국어: {kors[i]}, 영어: {engs[i]}, 수학: {mats[i]}')
+
+    elif menu == '3':
+        print('성적 데이터 상세 조회')
+
+    elif menu == '4':
+        print('성적 데이터 수정')
+    elif menu == '5':
+        print('성적 데이터 삭제')
+    elif menu == '0':
+        print('프로그램 종료!')
+        sys.exit(0)
+    else:
+        print('메뉴를 잘못 선택하셨습니다')
 
 
-# 성적 처리
-for i in range(len(names)):
-    tots.append(kors[i] + engs[i] + mats[i])
-    avgs.append(tots[i] / 3)
-    avg = avgs[len(avgs)-1]
-    grd = '수' if avg >= 90 else \
-          '우' if avg >= 80 else \
-          '미' if avg >= 70 else \
-          '양' if avg >= 60 else '가'
-    grds.append(grd)
-
-# 결과 출력
-for i in range(len(names)):
-    print(f'이름 : {names[i]}, 국어: {kors[i]}, 영어: {engs[i]}, 수학: {mats[i]}')
-    print(f'총점: {tots[i]}, 평균: {avgs[i]:.1f}, 학점 : {grds[i]}')
-
-# 반복 수행 시 이터러블 객체가 필요없는 경우 변수명 대신 _ 를 사용
-for _ in range(10):
-    print('Hello, World!!')
 
