816249152536da7bc7b00c1f699f8e90404ae927
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-01-02 10:49:22 +0900

    향상된 모듈 사용법

---
+++ poppo/math.py
@@ -0,0 +1,12 @@
+# 더하기 연산
+def add(a,b):
+    print(' 덧셈 연산 결과 : ', a + b)
+# 빼기 연산
+def sub(a,b):
+    print(' 뺄셈 연산 결과 : ', a - b)
+# 곱하기 연산
+def mul(a,b):
+    print(' 곱셈 연산 결과 : ', a * b)
+# 나누기 연산
+def div(a,b):
+    print(' 나눗셈 연산 결과 : ', a / b)
