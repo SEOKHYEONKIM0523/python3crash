1853506fb9cb9896e92322adf11e3d0f7780d90e
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-01-03 10:30:50 +0900

    사원 등록 프로그램 V1 - 사원 추가

--- poppo/empv1.py
+++ poppo/empv1.py
@@ -23,8 +23,33 @@ def show_menu():
     menu = input('=> 메뉴를 선택하세요 : ')
     return menu
 
+
+def input_employee():
+    emp = {}
+
+    emp['empno'] = input('사원 번호를 입력하시오. ')
+    emp['fname'] = input('이름을 입력하시오. ')
+    emp['lname'] = input('성을 입력하시오. ')
+    emp['email'] = input('이메일을 입력하시오. ')
+    emp['hdate'] = input('입사일을 입력하시오. ')
+    emp['jobid'] = input('직책을 입력하시오. ')
+    emp['sal'] = input('급여를 입력하시오. ')
+    emp['deptid'] = input('부서번호를 입력하시오. ')
+
+    return emp
+
+def save_employee(emp):
+    row = (f"{emp['empno']}, {emp['fname']}, {emp['lname']}, {emp['email']}, "
+           f"{emp['hdate']}, {emp['jobid']}, {emp['sal']}, {emp['deptid']}\n")
+    with open('employees.csv','a') as f:
+        f.write(row)
+
+
+
 def add_employee():
     print('사원 정보를 등록합니다.')
+    emp = input_employee()
+    save_employee(emp)
 
 
 
