032a6d73dbba7b9eddc2fc7e9a3f8657911dbf85
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-01-04 11:15:45 +0900

    성적 처리 프로그램 V6c - 성적 삭제

--- poppo/sjv6c.py
+++ poppo/sjv6c.py
@@ -139,8 +139,24 @@ def modify_sungjuk():
         print('찾는 데이터가 없습니다!')
 
 def remove_sungjuk():
-    pass
+    name = input('삭제할 학생 이름은?')
 
+    # 삭제할 데이터를 찾음
+    data = None
+    for sj in items:
+        if sj['name'] == name:
+            data = sj
+            break
+    # 삭제할 데이터를 찾았다면
+    if data:
+        confirm = input('정말로 삭제하시겠습니까? (yes/no)')
+        if confirm == 'yes':
+            items.remove(data)
+            sjs['response']['body']['totalCount'] -= 1
+            print(f'{name}의 데이터가 삭제되었습니다')
+            flush_sungjuk()
+        else:
+            print('삭제가 취소되었습니다')
 
 def exit_program():
     print('프로그램 종료!')
