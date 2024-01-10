c8341ff7c71d87c17349a820cc9ac39c9e68d302
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-01-10 14:09:28 +0900

    도서관리 프로그램 v1 - insert_book 구현

--- poppo/BookDAO.py
+++ poppo/BookDAO.py
@@ -1,8 +1,30 @@
+import poppo.dbinfo2 as dbinfo
+
+insertsql = (' insert into book (bkname, author, publisher, pubdate, retail, price, pctoff, mileage)'
+             ' values (%s,%s,%s,%s,%s,%s,%s,%s) ')
+selectsql = 'select bkno, bkname, author, publisher, price from book'
+selectonesql = 'select * from book where bkno =1'
+updatesql = (' update book set bkname = %s, author = %s, publisher = %s,'
+             'pubdate = %s, retail = %s, pctoff = %s where bkno = %s ')
+
+
 class BookDAO:
     @staticmethod
-    def insert_book():
-        pass
+    def insert_book(bk):
+        """
+        입력받은 도서데이터를 book테이블에 저장
+        :param bk: 도서데이터
+        :return: 저장된 데이터 건수
+        """
+        cursor, conn = dbinfo.openConn()
+        params = [bk.bkname, bk.author, bk.publisher, bk.pubdate, int(bk.retail), int(bk.price), int(bk.pctoff), int(bk.mileage)]
+        cursor.execute(insertsql,params)
+        conn.commit()
 
+        rowcnt = cursor.rowcount
+
+        dbinfo.closeConn(cursor,conn)
+        return rowcnt
 
     @staticmethod
     def select_book():
